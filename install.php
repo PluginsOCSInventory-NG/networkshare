74532af098d81f6ae615b3f1a0d90fd212df7a2c
Stéphane <steph78630@gmail.com>
2017-07-24 20:50:52 +0200

    Add files via upload

--- install.php
+++ install.php
@@ -14,15 +14,15 @@ $object = new plugins;
 $object -> add_cd_entry("networkshare","other");
 
 $object -> sql_query("CREATE TABLE IF NOT EXISTS `networkshare` (
-                      `ID` INT(11) NOT NULL AUTO_INCREMENT,
-                      `HARDWARE_ID` INT(11) NOT NULL,
-                      `DRIVE` VARCHAR(255) DEFAULT NULL,
-                      `PATH` VARCHAR(255) DEFAULT NULL,
-                      `SIZE` VARCHAR(255) DEFAULT NULL,
-                      `FREESPACE` VARCHAR(255) DEFAULT NULL,
-                      `QUOTA` VARCHAR(255) DEFAULT NULL,
-                      PRIMARY KEY  (`ID`,`HARDWARE_ID`)
-                      ) ENGINE=INNODB;");
+  `ID` INT(11) NOT NULL AUTO_INCREMENT,
+  `HARDWARE_ID` INT(11) NOT NULL,
+  `DRIVE` VARCHAR(255) DEFAULT NULL,
+  `PATH` VARCHAR(255) DEFAULT NULL,
+  `SIZE` VARCHAR(255) DEFAULT NULL,
+  `FREESPACE` VARCHAR(255) DEFAULT NULL,
+  `QUOTA` VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY  (`ID`,`HARDWARE_ID`)
+) ENGINE=INNODB;");
 
 }
 
