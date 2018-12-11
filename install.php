a5232f8c50bde02ad7a4bc3c3f02657f878caad0
Charlene Auger <charlene.auger@factorfx.com>
2018-12-11 16:48:10 +0100

    Update plugin for extension engine

--- install.php
+++ install.php
@@ -1,37 +1,37 @@
 <?php
-function plugin_version_networkshare()
+
+/**
+ * This function is called on installation and is used to create database schema for the plugin
+ */
+function extension_install_networkshare()
 {
-return array('name' => 'Network Share',
-'version' => '2.0',
-'author'=> 'Valentin DEVILLE, Stephane PAUTREL',
-'license' => 'GPLv2',
-'verMinOcs' => '2.2');
+    $commonObject = new ExtensionCommon;
+
+    $commonObject -> sqlQuery("CREATE TABLE IF NOT EXISTS `networkshare` (
+                              `ID` INT(11) NOT NULL AUTO_INCREMENT,
+                              `HARDWARE_ID` INT(11) NOT NULL,
+                              `DRIVE` VARCHAR(255) DEFAULT NULL,
+                              `PATH` VARCHAR(255) DEFAULT NULL,
+                              `SIZE` VARCHAR(255) DEFAULT NULL,
+                              `FREESPACE` VARCHAR(255) DEFAULT NULL,
+                              `QUOTA` VARCHAR(255) DEFAULT NULL,
+                              PRIMARY KEY  (`ID`,`HARDWARE_ID`)
+                              ) ENGINE=INNODB;");
 }
 
-function plugin_init_networkshare()
+/**
+ * This function is called on removal and is used to destroy database schema for the plugin
+ */
+function extension_delete_networkshare()
 {
-$object = new plugins;
-$object -> add_cd_entry("networkshare","other");
-
-$object -> sql_query("CREATE TABLE IF NOT EXISTS `networkshare` (
-  `ID` INT(11) NOT NULL AUTO_INCREMENT,
-  `HARDWARE_ID` INT(11) NOT NULL,
-  `DRIVE` VARCHAR(255) DEFAULT NULL,
-  `PATH` VARCHAR(255) DEFAULT NULL,
-  `SIZE` VARCHAR(255) DEFAULT NULL,
-  `FREESPACE` VARCHAR(255) DEFAULT NULL,
-  `QUOTA` VARCHAR(255) DEFAULT NULL,
-  PRIMARY KEY  (`ID`,`HARDWARE_ID`)
-) ENGINE=INNODB;");
-
+    $commonObject = new ExtensionCommon;
+    $commonObject -> sqlQuery("DROP TABLE IF EXISTS `networkshare`");
 }
 
-function plugin_delete_networkshare()
+/**
+ * This function is called on plugin upgrade
+ */
+function extension_upgrade_networkshare()
 {
-$object = new plugins;
-$object -> del_cd_entry("networkshare");
-$object -> sql_query("DROP TABLE `networkshare`;");
 
 }
-
-?>
